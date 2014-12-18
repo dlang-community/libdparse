04100afe5f42c3f38af8af3c7ede4b62e0630c78
Hackerpilot <briancschott@gmail.com>
2014-12-17 16:23:39 -0800

    Improve fuzzer output

--- test/fuzzer.sh
+++ test/fuzzer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+TOTALRUNS=10000
 
 echo "Compiling fuzzer"
 dmd fuzzer.d -I../src/ ../src/std/d/*.d ../src/std/*.d || exit 1
@@ -6,10 +6,13 @@ echo "Done"
 echo "Compiling parser"
 dmd -O -inline -g tester.d ../src/std/*.d ../src/std/d/*.d  -I../src/ || exit 1
 echo "Done"
-for i in $(seq 0 100000); do
+printf "           "
+for i in $(seq $TOTALRUNS); do
+	printf "\b\b\b\b\b\b\b\b\b\b\b"
+	printf "%5d/%5d" $i $TOTALRUNS
 	./fuzzer > tokens.txt
 	./tester tokens.txt > output.txt 2>&1
 	if [ $? -eq 139 ]; then echo "Segfaulted..."; sed -e "s/ /\\n/g" tokens.txt > tokens-newlines.txt; exit 1; fi
 	grep ception output.txt && sed -e "s/ /\\n/g" tokens.txt > tokens-newlines.txt && exit 1;
-	echo "$i"
 done
+printf "\n"
--- test/tester.d
+++ test/tester.d
@@ -10,290 +10,290 @@ int errorCount = 0;
 int warningCount = 0;
 
 void messageFunction(string fileName, size_t line, size_t column,
-	string message, bool isError)
+    string message, bool isError)
 {
-	if (isError)
-	{
-		errorCount++;
-		stderr.writefln("%s(%d:%d)[error]: %s", fileName, line, column, message);
-	}
-	else
-	{
-		stdout.writefln("%s(%d:%d)[warn ]: %s", fileName, line, column, message);
-		warningCount++;
-	}
+    if (isError)
+    {
+        errorCount++;
+        stderr.writefln("%s(%d:%d)[error]: %s", fileName, line, column, message);
+    }
+    else
+    {
+        stdout.writefln("%s(%d:%d)[warn ]: %s", fileName, line, column, message);
+        warningCount++;
+    }
 }
 
 void testTokenChecks()
 {
-	foreach (IdType i; 0 .. IdType.max)
-	{
-		switch (i)
-		{
-		case tok!"int":
-		case tok!"uint":
-		case tok!"double":
-		case tok!"idouble":
-		case tok!"float":
-		case tok!"ifloat":
-		case tok!"short":
-		case tok!"ushort":
-		case tok!"long":
-		case tok!"ulong":
-		case tok!"char":
-		case tok!"wchar":
-		case tok!"dchar":
-		case tok!"bool":
-		case tok!"void":
-		case tok!"cent":
-		case tok!"ucent":
-		case tok!"real":
-		case tok!"ireal":
-		case tok!"byte":
-		case tok!"ubyte":
-		case tok!"cdouble":
-		case tok!"cfloat":
-		case tok!"creal":
-			assert (isBasicType(i));
-			assert (!isNumberLiteral(i));
-			assert (!isOperator(i));
-			assert (!isKeyword(i));
-			assert (!isStringLiteral(i));
-			assert (!isProtection(i));
-			break;
-		case tok!"doubleLiteral":
-		case tok!"floatLiteral":
-		case tok!"idoubleLiteral":
-		case tok!"ifloatLiteral":
-		case tok!"intLiteral":
-		case tok!"longLiteral":
-		case tok!"realLiteral":
-		case tok!"irealLiteral":
-		case tok!"uintLiteral":
-		case tok!"ulongLiteral":
-			assert (!isBasicType(i));
-			assert (isNumberLiteral(i));
-			assert (!isOperator(i));
-			assert (!isKeyword(i));
-			assert (!isStringLiteral(i));
-			assert (!isProtection(i));
-			break;
-		case tok!",":
-		case tok!".":
-		case tok!"..":
-		case tok!"...":
-		case tok!"/":
-		case tok!"/=":
-		case tok!"!":
-		case tok!"!<":
-		case tok!"!<=":
-		case tok!"!<>":
-		case tok!"!<>=":
-		case tok!"!=":
-		case tok!"!>":
-		case tok!"!>=":
-		case tok!"$":
-		case tok!"%":
-		case tok!"%=":
-		case tok!"&":
-		case tok!"&&":
-		case tok!"&=":
-		case tok!"(":
-		case tok!")":
-		case tok!"*":
-		case tok!"*=":
-		case tok!"+":
-		case tok!"++":
-		case tok!"+=":
-		case tok!"-":
-		case tok!"--":
-		case tok!"-=":
-		case tok!":":
-		case tok!";":
-		case tok!"<":
-		case tok!"<<":
-		case tok!"<<=":
-		case tok!"<=":
-		case tok!"<>":
-		case tok!"<>=":
-		case tok!"=":
-		case tok!"==":
-		case tok!"=>":
-		case tok!">":
-		case tok!">=":
-		case tok!">>":
-		case tok!">>=":
-		case tok!">>>":
-		case tok!">>>=":
-		case tok!"?":
-		case tok!"@":
-		case tok!"[":
-		case tok!"]":
-		case tok!"^":
-		case tok!"^=":
-		case tok!"^^":
-		case tok!"^^=":
-		case tok!"{":
-		case tok!"|":
-		case tok!"|=":
-		case tok!"||":
-		case tok!"}":
-		case tok!"~":
-		case tok!"~=":
-			assert (!isBasicType(i));
-			assert (!isNumberLiteral(i));
-			assert (isOperator(i));
-			assert (!isKeyword(i));
-			assert (!isStringLiteral(i));
-			assert (!isProtection(i));
-			break;
-		case tok!"abstract":
-		case tok!"alias":
-		case tok!"align":
-		case tok!"asm":
-		case tok!"assert":
-		case tok!"auto":
-		case tok!"body":
-		case tok!"break":
-		case tok!"case":
-		case tok!"cast":
-		case tok!"catch":
-		case tok!"class":
-		case tok!"const":
-		case tok!"continue":
-		case tok!"debug":
-		case tok!"default":
-		case tok!"delegate":
-		case tok!"delete":
-		case tok!"deprecated":
-		case tok!"do":
-		case tok!"else":
-		case tok!"enum":
-		case tok!"extern":
-		case tok!"false":
-		case tok!"final":
-		case tok!"finally":
-		case tok!"for":
-		case tok!"foreach":
-		case tok!"foreach_reverse":
-		case tok!"function":
-		case tok!"goto":
-		case tok!"if":
-		case tok!"immutable":
-		case tok!"import":
-		case tok!"in":
-		case tok!"inout":
-		case tok!"interface":
-		case tok!"invariant":
-		case tok!"is":
-		case tok!"lazy":
-		case tok!"macro":
-		case tok!"mixin":
-		case tok!"module":
-		case tok!"new":
-		case tok!"nothrow":
-		case tok!"null":
-		case tok!"out":
-		case tok!"override":
-		case tok!"pragma":
-		case tok!"pure":
-		case tok!"ref":
-		case tok!"return":
-		case tok!"scope":
-		case tok!"shared":
-		case tok!"static":
-		case tok!"struct":
-		case tok!"super":
-		case tok!"switch":
-		case tok!"synchronized":
-		case tok!"template":
-		case tok!"this":
-		case tok!"throw":
-		case tok!"true":
-		case tok!"try":
-		case tok!"typedef":
-		case tok!"typeid":
-		case tok!"typeof":
-		case tok!"union":
-		case tok!"unittest":
-		case tok!"version":
-		case tok!"volatile":
-		case tok!"while":
-		case tok!"with":
-		case tok!"__DATE__":
-		case tok!"__EOF__":
-		case tok!"__FILE__":
-		case tok!"__FUNCTION__":
-		case tok!"__gshared":
-		case tok!"__LINE__":
-		case tok!"__MODULE__":
-		case tok!"__parameters":
-		case tok!"__PRETTY_FUNCTION__":
-		case tok!"__TIME__":
-		case tok!"__TIMESTAMP__":
-		case tok!"__traits":
-		case tok!"__vector":
-		case tok!"__VENDOR__":
-		case tok!"__VERSION__":
-			assert (!isBasicType(i));
-			assert (!isNumberLiteral(i));
-			assert (!isOperator(i));
-			assert (isKeyword(i));
-			assert (!isStringLiteral(i));
-			assert (!isProtection(i));
-			break;
-		case tok!"dstringLiteral":
-		case tok!"stringLiteral":
-		case tok!"wstringLiteral":
-			assert (!isBasicType(i));
-			assert (!isNumberLiteral(i));
-			assert (!isOperator(i));
-			assert (!isKeyword(i));
-			assert (isStringLiteral(i));
-			assert (!isProtection(i));
-			break;
-		case tok!"export":
-		case tok!"package":
-		case tok!"private":
-		case tok!"public":
-		case tok!"protected":
-			assert (!isBasicType(i));
-			assert (!isNumberLiteral(i));
-			assert (!isOperator(i));
-			assert (isKeyword(i));
-			assert (!isStringLiteral(i));
-			assert (isProtection(i));
-			break;
-		default:
-			assert (!isBasicType(i));
-			assert (!isNumberLiteral(i));
-			assert (!isOperator(i));
-			assert (!isKeyword(i));
-			assert (!isStringLiteral(i));
-			assert (!isProtection(i));
-			break;
-		}
-	}
+    foreach (IdType i; 0 .. IdType.max)
+    {
+        switch (i)
+        {
+        case tok!"int":
+        case tok!"uint":
+        case tok!"double":
+        case tok!"idouble":
+        case tok!"float":
+        case tok!"ifloat":
+        case tok!"short":
+        case tok!"ushort":
+        case tok!"long":
+        case tok!"ulong":
+        case tok!"char":
+        case tok!"wchar":
+        case tok!"dchar":
+        case tok!"bool":
+        case tok!"void":
+        case tok!"cent":
+        case tok!"ucent":
+        case tok!"real":
+        case tok!"ireal":
+        case tok!"byte":
+        case tok!"ubyte":
+        case tok!"cdouble":
+        case tok!"cfloat":
+        case tok!"creal":
+            assert (isBasicType(i));
+            assert (!isNumberLiteral(i));
+            assert (!isOperator(i));
+            assert (!isKeyword(i));
+            assert (!isStringLiteral(i));
+            assert (!isProtection(i));
+            break;
+        case tok!"doubleLiteral":
+        case tok!"floatLiteral":
+        case tok!"idoubleLiteral":
+        case tok!"ifloatLiteral":
+        case tok!"intLiteral":
+        case tok!"longLiteral":
+        case tok!"realLiteral":
+        case tok!"irealLiteral":
+        case tok!"uintLiteral":
+        case tok!"ulongLiteral":
+            assert (!isBasicType(i));
+            assert (isNumberLiteral(i));
+            assert (!isOperator(i));
+            assert (!isKeyword(i));
+            assert (!isStringLiteral(i));
+            assert (!isProtection(i));
+            break;
+        case tok!",":
+        case tok!".":
+        case tok!"..":
+        case tok!"...":
+        case tok!"/":
+        case tok!"/=":
+        case tok!"!":
+        case tok!"!<":
+        case tok!"!<=":
+        case tok!"!<>":
+        case tok!"!<>=":
+        case tok!"!=":
+        case tok!"!>":
+        case tok!"!>=":
+        case tok!"$":
+        case tok!"%":
+        case tok!"%=":
+        case tok!"&":
+        case tok!"&&":
+        case tok!"&=":
+        case tok!"(":
+        case tok!")":
+        case tok!"*":
+        case tok!"*=":
+        case tok!"+":
+        case tok!"++":
+        case tok!"+=":
+        case tok!"-":
+        case tok!"--":
+        case tok!"-=":
+        case tok!":":
+        case tok!";":
+        case tok!"<":
+        case tok!"<<":
+        case tok!"<<=":
+        case tok!"<=":
+        case tok!"<>":
+        case tok!"<>=":
+        case tok!"=":
+        case tok!"==":
+        case tok!"=>":
+        case tok!">":
+        case tok!">=":
+        case tok!">>":
+        case tok!">>=":
+        case tok!">>>":
+        case tok!">>>=":
+        case tok!"?":
+        case tok!"@":
+        case tok!"[":
+        case tok!"]":
+        case tok!"^":
+        case tok!"^=":
+        case tok!"^^":
+        case tok!"^^=":
+        case tok!"{":
+        case tok!"|":
+        case tok!"|=":
+        case tok!"||":
+        case tok!"}":
+        case tok!"~":
+        case tok!"~=":
+            assert (!isBasicType(i));
+            assert (!isNumberLiteral(i));
+            assert (isOperator(i));
+            assert (!isKeyword(i));
+            assert (!isStringLiteral(i));
+            assert (!isProtection(i));
+            break;
+        case tok!"abstract":
+        case tok!"alias":
+        case tok!"align":
+        case tok!"asm":
+        case tok!"assert":
+        case tok!"auto":
+        case tok!"body":
+        case tok!"break":
+        case tok!"case":
+        case tok!"cast":
+        case tok!"catch":
+        case tok!"class":
+        case tok!"const":
+        case tok!"continue":
+        case tok!"debug":
+        case tok!"default":
+        case tok!"delegate":
+        case tok!"delete":
+        case tok!"deprecated":
+        case tok!"do":
+        case tok!"else":
+        case tok!"enum":
+        case tok!"extern":
+        case tok!"false":
+        case tok!"final":
+        case tok!"finally":
+        case tok!"for":
+        case tok!"foreach":
+        case tok!"foreach_reverse":
+        case tok!"function":
+        case tok!"goto":
+        case tok!"if":
+        case tok!"immutable":
+        case tok!"import":
+        case tok!"in":
+        case tok!"inout":
+        case tok!"interface":
+        case tok!"invariant":
+        case tok!"is":
+        case tok!"lazy":
+        case tok!"macro":
+        case tok!"mixin":
+        case tok!"module":
+        case tok!"new":
+        case tok!"nothrow":
+        case tok!"null":
+        case tok!"out":
+        case tok!"override":
+        case tok!"pragma":
+        case tok!"pure":
+        case tok!"ref":
+        case tok!"return":
+        case tok!"scope":
+        case tok!"shared":
+        case tok!"static":
+        case tok!"struct":
+        case tok!"super":
+        case tok!"switch":
+        case tok!"synchronized":
+        case tok!"template":
+        case tok!"this":
+        case tok!"throw":
+        case tok!"true":
+        case tok!"try":
+        case tok!"typedef":
+        case tok!"typeid":
+        case tok!"typeof":
+        case tok!"union":
+        case tok!"unittest":
+        case tok!"version":
+        case tok!"volatile":
+        case tok!"while":
+        case tok!"with":
+        case tok!"__DATE__":
+        case tok!"__EOF__":
+        case tok!"__FILE__":
+        case tok!"__FUNCTION__":
+        case tok!"__gshared":
+        case tok!"__LINE__":
+        case tok!"__MODULE__":
+        case tok!"__parameters":
+        case tok!"__PRETTY_FUNCTION__":
+        case tok!"__TIME__":
+        case tok!"__TIMESTAMP__":
+        case tok!"__traits":
+        case tok!"__vector":
+        case tok!"__VENDOR__":
+        case tok!"__VERSION__":
+            assert (!isBasicType(i));
+            assert (!isNumberLiteral(i));
+            assert (!isOperator(i));
+            assert (isKeyword(i));
+            assert (!isStringLiteral(i));
+            assert (!isProtection(i));
+            break;
+        case tok!"dstringLiteral":
+        case tok!"stringLiteral":
+        case tok!"wstringLiteral":
+            assert (!isBasicType(i));
+            assert (!isNumberLiteral(i));
+            assert (!isOperator(i));
+            assert (!isKeyword(i));
+            assert (isStringLiteral(i));
+            assert (!isProtection(i));
+            break;
+        case tok!"export":
+        case tok!"package":
+        case tok!"private":
+        case tok!"public":
+        case tok!"protected":
+            assert (!isBasicType(i));
+            assert (!isNumberLiteral(i));
+            assert (!isOperator(i));
+            assert (isKeyword(i));
+            assert (!isStringLiteral(i));
+            assert (isProtection(i));
+            break;
+        default:
+            assert (!isBasicType(i));
+            assert (!isNumberLiteral(i));
+            assert (!isOperator(i));
+            assert (!isKeyword(i));
+            assert (!isStringLiteral(i));
+            assert (!isProtection(i));
+            break;
+        }
+    }
 }
 
 int main(string[] args)
 {
-	version (D_Coverage) testTokenChecks();
-	enforce(args.length > 1, "Must specifiy at least one D file");
-	foreach (arg; args[1 .. $])
-	{
-		auto f = File(arg);
-		immutable ulong fileSize = f.size();
-		ubyte[] fileBytes = new ubyte[](fileSize);
-		enforce(f.rawRead(fileBytes).length == fileSize);
-		StringCache cache = StringCache(StringCache.defaultBucketCount);
-		LexerConfig config;
-		config.stringBehavior = StringBehavior.source;
-		config.fileName = arg;
-		const(Token)[] tokens = getTokensForParser(fileBytes, config, &cache);
-		parseModule(tokens, arg, null, &messageFunction);
-	}
-	writefln("Finished parsing with %d errors and %d warnings.",
-			errorCount, warningCount);
-	return errorCount == 0 ? 0 : 1;
+    version (D_Coverage) testTokenChecks();
+    enforce(args.length > 1, "Must specifiy at least one D file");
+    foreach (arg; args[1 .. $])
+    {
+        auto f = File(arg);
+        immutable ulong fileSize = f.size();
+        ubyte[] fileBytes = new ubyte[](fileSize);
+        enforce(f.rawRead(fileBytes).length == fileSize);
+        StringCache cache = StringCache(StringCache.defaultBucketCount);
+        LexerConfig config;
+        config.stringBehavior = StringBehavior.source;
+        config.fileName = arg;
+        const(Token)[] tokens = getTokensForParser(fileBytes, config, &cache);
+        parseModule(tokens, arg, null, &messageFunction);
+    }
+    writefln("Finished parsing with %d errors and %d warnings.",
+            errorCount, warningCount);
+    return errorCount == 0 ? 0 : 1;
 }
