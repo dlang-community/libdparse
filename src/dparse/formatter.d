8f016a213839fa2734eef1d86eb3b387fd8dcf00
Sebastian Wilzbach <seb@wilzba.ch>
2017-02-22 16:47:14 +0100

    Formatter: fix fields of primaryExpression
    For example, the `intLiteral` here was omitted before:
    
    in:  assert(float(0))
    out: assert(float)

--- src/dparse/formatter.d
+++ src/dparse/formatter.d
@@ -2341,12 +2341,14 @@ class Formatter(Sink)
         AssocArrayLiteral assocArrayLiteral;
         Expression expression;
         IsExpression isExpression;
-        LambdaExpression lambdaExpression;
         FunctionLiteralExpression functionLiteralExpression;
         TraitsExpression traitsExpression;
         MixinExpression mixinExpression;
         ImportExpression importExpression;
         Vector vector;
+        Type type;
+        Token typeConstructor;
+        Arguments arguments;
         **/
 
         with(primaryExpression)
@@ -2379,6 +2381,8 @@ class Formatter(Sink)
             else if (mixinExpression) format(mixinExpression);
             else if (importExpression) format(importExpression);
             else if (vector) format(vector);
+            else if (type) format(type);
+            else if (arguments) format(arguments);
         }
     }
 
