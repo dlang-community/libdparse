22ebe995e539d0d63cb046fb2914bfcae924fb70
Basile Burg <basile.b@gmx.com>
2019-05-14 03:08:17 +0200

    fix #360 - parse error when `body` is used in expression

--- src/dparse/parser.d
+++ src/dparse/parser.d
@@ -5224,8 +5224,6 @@ class Parser
             node.dot = advance();
             goto case;
         case tok!"identifier":
-            if (current.text == "body")
-                goto case tok!"do";
             if (peekIs(tok!"=>"))
                 mixin(parseNodeQ!(`node.functionLiteralExpression`, `FunctionLiteralExpression`));
             else
